fcb0545dc929d652cee41a5f6fc1dfb942b899e8
Vipul Kumar Chauhan <vc252@snu.edu.in>
2024-07-29 18:05:22 +0530

    make the basic console game

---
+++ script.js
@@ -0,0 +1,108 @@
+//so we are building a tic tac toe game
+
+const Board = (function() {
+    //this would be the tic tac toe matrix
+    const board = [
+        [null,null,null],
+        [null,null,null],
+        [null,null,null]
+    ]
+    
+    const display = ()=>{
+        board.forEach((row)=>console.log([...row]));
+    }
+
+    const move = (player,row,col)=>{
+        if (board[row][col]!=null) {
+            alert("not valid");
+            return false;
+        } 
+        board[row][col] = player.char;
+        return true;
+    }
+
+    const checkWin = (char)=>{
+        //first we check the row
+        for (row of board) {
+            if (!row.includes(null) && (new Set(row)).size===1) return true;
+        }
+        //we check for columns
+        for (let col=0; col<3; col++) {
+            if ((board[0][col]!=null && board[1][col]!=null && board[2][col]!=null) &&
+                (board[0][col] === board[1][col] && board[1][col]===board[2][col])) {
+                return true;
+            }
+        }
+        //we check for the diagnols
+        if ((board[0][0]!=null && board[1][1]!=null && board[2][2]!=null) &&
+            (board[0][0] === board[1][1] && board[1][1] === board[2][2])) {
+            return true;
+        }
+        if ((board[0][2]!=null && board[1][1]!=null && board[2][0]!=null) &&
+            (board[0][2] === board[1][1] && board[1][1] === board[2][0])) {
+            return true;
+        }
+        //if nothing matches 
+        return false;
+    }
+
+    const refresh = ()=>{
+        for (let row=0; row<3; row++) {
+            for (let col=0; col<3; col++) {
+                board[row][col] = null;
+            }
+        }
+    }
+
+    return {move,checkWin,display,refresh};
+
+})();
+
+//now we need the players
+function createPlayer(name,char) {
+    return {
+        name,
+        char
+    }
+}
+
+function playRound(player1,player2) {
+    let currentPlayer = player1;
+    for (let i = 0; i < 9; i++) {
+        let validMove = false;
+        while (!validMove) {
+            let row = prompt(`${currentPlayer.name} Enter row`);
+            let col = prompt(`${currentPlayer.name} Enter col`);
+            validMove = Board.move(currentPlayer,parseInt(row),parseInt(col));
+            if (!validMove) {
+                console.log('Invalid move. Try again.');
+            }
+        }
+      
+        Board.display();
+        if (Board.checkWin(currentPlayer.char)) {
+            console.log(`${currentPlayer.name} wins`);
+            return;
+        }
+        currentPlayer = currentPlayer===player1 ? player2 : player1;
+    }
+    console.log("draw");
+}
+
+//now the game round
+function startMain() {
+    let playerName = prompt("Enter player 1 name","player1");
+    let playerChar = prompt(`Enter the char choice for ${playerName}`,"0");
+    let player1 = createPlayer(playerName,playerChar);
+
+    playerName = prompt("Enter player 2 name","player2");
+    playerChar = prompt(`Enter the char choice for ${playerName}`,"1");
+    let player2= createPlayer(playerName,playerChar);
+
+    do {
+        playRound(player1,player2);
+        Board.refresh();
+    } while (prompt("Do you want to play again(yes/no)?", "no").toLowerCase() === "yes");
+}
+
+startMain();
